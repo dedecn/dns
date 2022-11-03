03121a86333f8782125597a6ff41e69334679641
dedecn <dedecn@qq.com>
2022-11-03 23:45:58 +0800

    BOUND_IF only available while reuse port

--- listen_reuseport.go
+++ listen_reuseport.go
@@ -18,6 +18,11 @@ func reuseportControl(network, address string, c syscall.RawConn) error {
 	var opErr error
 	err := c.Control(func(fd uintptr) {
 		opErr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+		if opErr == nil {
+			if IfIdx >= 0 {
+				opErr = syscall.SetsockoptInt(int(fd), unix.IPPROTO_IP, unix.IP_BOUND_IF, IfIdx)
+			}
+		}
 	})
 	if err != nil {
 		return err
@@ -31,41 +36,12 @@ var IfIdx int = -1
 func SetIfIdx(ifIdx int) {
 	IfIdx = ifIdx
 }
-func controlBoundIf(network, address string, conn syscall.RawConn) error {
-	if IfIdx < 0 {
-		return nil
-	}
-	var operr error
-	if err := conn.Control(func(fd uintptr) {
-		operr = syscall.SetsockoptInt(int(fd), unix.IPPROTO_IP, unix.IP_BOUND_IF, IfIdx)
-	}); err != nil {
-		return err
-	}
-	return operr
-}
 
-type controlFuncType func(network, address string, c syscall.RawConn) error
-
-func composeControlFunc(funcs []controlFuncType) controlFuncType {
-	return func(network, address string, c syscall.RawConn) error {
-		for _, f := range funcs {
-			if f == nil {
-				continue
-			}
-			err := f(network, address, c)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-}
 func listenTCP(network, addr string, reuseport bool) (net.Listener, error) {
 	var lc net.ListenConfig
 	if reuseport {
 		lc.Control = reuseportControl
 	}
-	lc.Control = composeControlFunc([]controlFuncType{lc.Control, controlBoundIf})
 	return lc.Listen(context.Background(), network, addr)
 }
 
@@ -74,6 +50,5 @@ func listenUDP(network, addr string, reuseport bool) (net.PacketConn, error) {
 	if reuseport {
 		lc.Control = reuseportControl
 	}
-	lc.Control = composeControlFunc([]controlFuncType{lc.Control, controlBoundIf})
 	return lc.ListenPacket(context.Background(), network, addr)
 }
